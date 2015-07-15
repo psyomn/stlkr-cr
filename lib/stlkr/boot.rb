413999a3a3fda37b4b2820a32ebc229c0ffc4503
psyomn <lethaljellybean@gmail.com>
2015-07-15 19:19:48 -0400

    core: add booting stuff

---
+++ lib/stlkr/boot.rb
@@ -0,0 +1,16 @@
+require 'stalker'
+require 'fileutils'
+
+module Stlkr
+# This takes care of some booting things that need to be executed each time the
+# software starts. An example would be checking for data files.
+class Boot
+  def self.up!
+    FileUtils.mkdir_p Stlkr::DATADIR
+    FileUtils.touch Stlkr::URIFILE
+  rescue => e
+    puts "problem creating datadir, or urifile"
+    pute e
+  end
+end
+end
