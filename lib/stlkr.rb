9e686f29a00e26bc3ab8f9c9cd01214804a91944
psyomn <lethaljellybean@gmail.com>
2015-07-17 04:35:32 -0400

    lib/stlkr: constants

--- lib/stlkr.rb
+++ lib/stlkr.rb
@@ -1,11 +1,15 @@
 require 'stlkr/version'
 require 'stlkr/boot'
 require 'stlkr/website'
+require 'stlkr/stalker_service'
 
 module Stlkr
   NAME = 'stlkr'
   HOME = ENV['HOME']
   CONFIGDIR = [HOME, '.config'].join('/') + '/'
-  DATADIR = [CONFIG, NAME].join('/') + '/'
+  DATADIR = [CONFIGDIR, NAME].join('/') + '/'
   URIFILE = [DATADIR, 'uris'].join('/')
+
+  # Seconds to check against a site each time
+  INTERVAL = 10
 end
