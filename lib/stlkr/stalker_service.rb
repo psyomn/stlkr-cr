e0d8334cf9f7f1b3027118c8b26fc2cd0e60b7ee
psyomn <lethaljellybean@gmail.com>
2015-07-17 04:37:02 -0400

    stalker_service: loop and notify on changes
    This is the part to be daemonized

--- lib/stlkr/stalker_service.rb
+++ lib/stlkr/stalker_service.rb
@@ -1,5 +1,42 @@
+require 'stlkr'
+require 'libnotify'
+
 module Stlkr
 # @author psyomn
 class StalkerService
+  def initialize
+    @done = false
+  end
+  def run
+    ws = load_websites
+    until @done do
+      ws = db_changed? ? load_websites : ws
+      puts 'refresh'
+      ws.each do |w|
+        prev = w.hashcode
+        print "Rechecking #{w} ..."
+        w.fetch!
+        new = w.hashcode
+
+        if prev != new
+          print " Site has changed!"
+          w.insert
+        end
+
+        puts
+      end
+      sleep Stlkr::INTERVAL
+    end
+  end
+
+  private
+
+  def db_changed?
+    true
+  end
+
+  def load_websites
+    Website.all
+  end
 end
 end
