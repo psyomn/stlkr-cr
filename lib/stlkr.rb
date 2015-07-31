9635e639ddf64ccd1c6ce6b641e1e58e40611d2a
psyomn <lethaljellybean@gmail.com>
2015-07-31 14:01:19 -0400

    lib/stlkr: update constants

--- lib/stlkr.rb
+++ lib/stlkr.rb
@@ -6,9 +6,17 @@ require 'stlkr/stalker_service'
 module Stlkr
   NAME = 'stlkr'
   HOME = ENV['HOME']
-  CONFIGDIR = [HOME, '.config'].join('/') + '/'
-  DATADIR = [CONFIGDIR, NAME].join('/') + '/'
-  URIFILE = [DATADIR, 'uris'].join('/')
+
+  URIFILE_NAME = 'uris'
+  TIMESTAMPFILE_NAME = 'lastupdate'
+
+  CONFIGDIR     = File.join(HOME, '.config').join
+  DATADIR       = File.join(CONFIGDIR, NAME).join
+  URIFILE       = File.join(DATADIR, URIFILE_NAME).join
+
+  # Whenever a user del/adds a new site to monitor, this file is updated with the
+  # current time.
+  TIMESTAMPFILE = File.join(DATADIR, TIMESTAMPFILE_NAME).join
 
   # Seconds to check against a site each time
   INTERVAL = 10
