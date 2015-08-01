d6f3c4351bcc3a93db4aae9326086036babf90aa
psyomn <lethaljellybean@gmail.com>
2015-08-01 19:33:53 -0400

    lib/stlkr: fix joins

--- lib/stlkr.rb
+++ lib/stlkr.rb
@@ -10,13 +10,13 @@ module Stlkr
   URIFILE_NAME = 'uris'
   TIMESTAMPFILE_NAME = 'lastupdate'
 
-  CONFIGDIR     = File.join(HOME, '.config').join
-  DATADIR       = File.join(CONFIGDIR, NAME).join
-  URIFILE       = File.join(DATADIR, URIFILE_NAME).join
+  CONFIGDIR     = File.join(HOME, '.config')
+  DATADIR       = File.join(CONFIGDIR, NAME)
+  URIFILE       = File.join(DATADIR, URIFILE_NAME)
 
   # Whenever a user del/adds a new site to monitor, this file is updated with the
   # current time.
-  TIMESTAMPFILE = File.join(DATADIR, TIMESTAMPFILE_NAME).join
+  TIMESTAMPFILE = File.join(DATADIR, TIMESTAMPFILE_NAME)
 
   # Seconds to check against a site each time
   INTERVAL = 10
