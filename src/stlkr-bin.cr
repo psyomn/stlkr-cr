1d9f1ddcaa4489679d42fd4eb0f953338b925fff
psyomn <lethaljellybean@gmail.com>
2017-12-30 09:24:17 -0500

    Translate stlkr-bin to crystal

--- src/stlkr-bin.cr
+++ src/stlkr-bin.cr
@@ -1,71 +1,59 @@
-#!/usr/bin/env ruby
-
-root = File.expand_path(File.join(File.dirname(__FILE__),'..'))
-if File.directory?(File.join(root,'.git'))
-  Dir.chdir(root) do
-    begin
-      require 'bundler/setup'
-    rescue LoadError => e
-      warn e.message
-      warn "Run `gem install bundler` to install Bundler"
-      exit(-1)
-    end
-  end
-end
-
-require 'stlkr'
-require 'optparse'
+# require 'stlkr'
+require "option_parser"
+require "./stlkr.cr"
 
 include Stlkr
 Boot.up!
 
-options = {}
-OptionParser.new do |opts|
+options = Hash(Symbol, String).new
+OptionParser.parse! do |opts|
   opts.banner = "stlkr: [options]"
 
-  opts.on('-a', '--add-link <url>', 'add a link to monitor') do |v|
+  opts.on("-a", "--add-link <url>", "add a link to monitor") do |v|
     options[:add_link] = v
   end
 
-  opts.on('-d', '--delete-link <link-id>', 'delete link from monitor list') do |v|
+  opts.on("-d", "--delete-link <link-id>", "delete link from monitor list") do |v|
     options[:delete_link] = v
   end
 
-  opts.on('-s', '--start-service', 'start the stalker service') do |v|
+  opts.on("-s", "--start-service", "start the stalker service") do |v|
     options[:start_service] = v
   end
 
-  opts.on('-x', '--stop-service', 'stop the stalker service') do |v|
+  opts.on("-x", "--stop-service", "stop the stalker service") do |v|
     options[:stop_service] = v
   end
 
-  opts.on('-l', '--ls-sites', 'list the websites monitored') do |v|
+  opts.on("-l", "--ls-sites", "list the websites monitored") do |v|
     options[:ls] = v
   end
 
-  opts.on('-u', '--username USERNAME', 'set the http username') do |v|
+  opts.on("-u", "--username USERNAME", "set the http username") do |v|
     options[:username] = v
   end
 
-  opts.on('-p', '--password PASSWORD', 'set the http password') do |v|
+  opts.on("-p", "--password PASSWORD", "set the http password") do |v|
     options[:password] = v
   end
-end.parse!
+end
 
-if options[:add_link]
-  w = Website.new(options[:add_link])
-  w.username = options[:username]
-  w.password = options[:password]
+if options[:add_link]?
+  url : String = options[:add_link].not_nil!
+  w = Website.new(url)
+  w.username = options[:username]?
+  w.password = options[:password]?
   w.insert
-elsif options[:delete_link]
-  w = Website.delete options[:delete_link]
-elsif options[:start_service]
-  th = Thread.new do
+elsif options[:delete_link]?
+  w = Website.delete(options[:delete_link])
+elsif options[:start_service]?
+  ch = Channel(Nil).new
+  spawn do
     ss = StalkerService.new
     ss.run
+    ch.send(nil)
   end
-  th.run
-  th.join
+  ch.receive
 elsif options[:ls]
   Website.all.each do |site|
     puts site.url
